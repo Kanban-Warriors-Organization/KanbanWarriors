7e3a364377621155a4b7347bfacd337dbc296e57
Sam-de-Ham <weisz.sam321@gmail.com>
2025-02-12 20:00:37 +0000

    Testing GitHub webhook

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run the administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cards.settings')
     try:
         from django.core.management import execute_from_command_line
