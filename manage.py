c43f46eb0abce31b04a80199d6803e891935260e
Sam-de-Ham <weisz.sam321@gmail.com>
2025-02-12 20:05:05 +0000

    Revert and test for webhook

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run the administrative tasks."""
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cards.settings')
     try:
         from django.core.management import execute_from_command_line
