bf0c949992be2f3194a9637022c94f5aee64c89b
Sam-de-Ham <weisz.sam321@gmail.com>
2025-03-22 01:58:30 +0000

    Revert "Postgresql"
    This reverts commit fd9872283bd23eff9caea18a4e9a54f54c4998ea.

--- cards/settings.py
+++ cards/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from decouple import config
 from pathlib import Path
 import os
 
@@ -79,13 +78,9 @@ WSGI_APPLICATION = "cards.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
